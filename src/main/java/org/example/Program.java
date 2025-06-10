583b967685e3d000e67b6bc4237f773b422f4238
Vinicius Macedo <viniciusp.macedo@gmail.com>
2025-06-09 21:09:02 -0300

    Department entity class

---
+++ src/main/java/org/example/Program.java
@@ -0,0 +1,11 @@
+package org.example;
+
+
+import org.example.domain.entities.Department;
+
+public class Program {
+    public static void main(String[] args) {
+        Department obj= new Department(1, "Books");
+        System.out.println(obj);
+    }
+}
---
+++ src/main/java/org/example/domain/entities/Department.java
@@ -0,0 +1,56 @@
+package org.example.domain.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Department implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private Integer id;
+    private String name;
+
+    public Department(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Department() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Department that = (Department) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Department{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
