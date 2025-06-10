f4337ad064e08cfba9dc423b12f53d0b7af99877
Vinicius Macedo <viniciusp.macedo@gmail.com>
2025-06-09 21:33:55 -0300

    DepartmentDao and SellerDao Interfaces

---
+++ src/main/java/org/example/model/dao/DepartmentDao.java
@@ -0,0 +1,17 @@
+package org.example.model.dao;
+
+import org.example.model.entities.Department;
+
+import java.util.List;
+
+public interface DepartmentDao {
+    void insert(Department department);
+
+    void update(Department department);
+
+    void deleteById(Integer id);
+
+    Department findById(Integer id);
+
+    List<Department> findAll();
+}
---
+++ src/main/java/org/example/model/dao/SellerDao.java
@@ -0,0 +1,14 @@
+package org.example.model.dao;
+
+import org.example.model.entities.Seller;
+
+import java.nio.channels.SeekableByteChannel;
+import java.util.List;
+
+public interface SellerDao {
+    void insert(Seller seller);
+    void update(Seller seller);
+    void deleteById(Integer id);
+    Seller findById(Integer id);
+    List<Seller> findAll();
+}
